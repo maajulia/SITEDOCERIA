88626d28aefbf59ebe48a42fc9bd86c4123c6949
maajulia <118356114+maajulia@users.noreply.github.com>
2024-09-01 13:01:20 -0400

    script.js

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ function mouseOut(){
 }
 
 ///
-var imgAtual = "assets/doceria.jpg";
-var imgAnterior = "assets/master.jpg";
+var imgAtual = "https://i.pinimg.com/564x/91/4b/df/914bdf5aef777b1b3b01dc1fcaa8a12f.jpg";
+var imgAnterior = "https://img.freepik.com/fotos-premium/tres-jovens-cozinheiras-em-uniformes-brancos-confiantemente-voltadas-para-a-camera_917664-161673.jpg";
  
 function trocar(){     document.getElementById("figura").src = imgAtual;
 let aux = imgAtual;
